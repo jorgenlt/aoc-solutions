f9d86f9ee965bdb248ae16654a69d5b0efa8964a
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2023-12-26 20:19:33 +0100

    setup styling, componentes, structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,51 @@
+import "./styles/app.scss";
+
+import Puzzle from "./components/Puzzle";
+
 const App = () => {
   return (
     <>
-      <h1>aoc-solutions</h1>
+      <header>
+        <div id="xmas--hat">
+          <img
+            src="/santa-hat.png"
+            alt="Santa's hat"
+            height={75}
+            style={{ marginRight: "20px" }}
+          />
+        </div>
+        <h1>
+          <span>Advent of Code 2023</span> - Solutions
+        </h1>
+      </header>
+
+      <main>
+        <div>
+          <h4>
+            <a href="#">Link to other years solutions</a>
+          </h4>
+          <h4>
+            Description + link to site:{" "}
+            <a
+              href="https://adventofcode.com/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Advent of Code
+            </a>
+            .
+          </h4>
+        </div>
+
+        <article>
+          <Puzzle
+            day={"1"}
+            title={"Hello"}
+            codePart1={"code part 1 block here"}
+            codePart2={"code part 2 here"}
+          />
+        </article>
+      </main>
     </>
   );
 };
---
+++ src/components/Puzzle.jsx
@@ -0,0 +1,31 @@
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { tomorrowNightBright } from "react-syntax-highlighter/dist/cjs/styles/hljs";
+
+const Puzzle = (props) => {
+  return (
+    <details className="puzzle">
+      <summary className="puzzle-summary">
+        <h2>
+          --- Day {props.day}: {props.title} ---
+        </h2>
+      </summary>
+      <div>
+        <span>
+          <a href="#" target="_blank">
+            Go to page
+          </a>
+        </span>
+        <h3>Part 1</h3>
+        <SyntaxHighlighter language="javascript" style={tomorrowNightBright} >
+          {props.codePart1}
+        </SyntaxHighlighter>
+        <h3>Part 2</h3>
+        <SyntaxHighlighter language="javascript" style={tomorrowNightBright}>
+          {props.codePart2}
+        </SyntaxHighlighter>
+      </div>
+    </details>
+  );
+};
+
+export default Puzzle;
